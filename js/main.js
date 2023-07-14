d316fbd3036ae15125cc1568cf3217999ed4f4b0
unknown <mosmack@gmail.com>
2023-07-14 16:41:31 -0300

    arrumando bugs javascript

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ function salvarJogo() {
   }
 
   if (!existe) {
-    itemAtual.id = listaJogos.length;
+    itemAtual.id = listaJogos[listaJogos.length -1] ? (listaJogos[listaJogos.length-1]).id + 1 : 0;
     listaJogos.push(itemAtual);
   }
 
@@ -67,7 +67,6 @@ function exibeJogosSalvos() {
   listaJogos.forEach(elemento => {
     const jogoString = elemento.jogo.toString().replaceAll(',', ' - ');
 
-    console.log(jogoString);
     criaElemento(jogoString, elemento.id);
   });
 }
@@ -105,89 +104,3 @@ function deletaElemento(tag, id) {
   exibeJogosSalvos();
   localStorage.setItem("listaJogos", JSON.stringify(listaJogos));
 }
-
-
-
-// AQUI DEU CERTO A PRIMEIRA VERSAO ABAIXO
-
-// const botaoCriaNovoJogo = document.getElementById('cria-novo-jogo');
-// const botaoSalvarJogo = document.getElementById('salvar-jogo');
-// const meusJogosLista = document.querySelector('.meus-jogos__lista');
-// const numero = document.getElementsByClassName('numero');
-// let novoJogo;
-// let novoJogoOrdenado;
-// const jogosSalvos = [];
-
-// botaoCriaNovoJogo.addEventListener('click', criaNovoJogo);
-// botaoSalvarJogo.addEventListener('click', salvarJogo);
-
-// function criaNovoJogo() {
-//   novoJogo = [];
-//   sorteiaNumeros();
-//   ordenaJogo();
-//   mostraJogoNaTela();
-// }
-
-// function sorteiaNumeros() {
-//   for(let i = 0; i < 6; i++) {
-//         const numeroAleatorio = Math.floor(Math.random()*60 + 1);
-    
-//         if(novoJogo.indexOf(numeroAleatorio) == -1) {
-//           novoJogo.push(numeroAleatorio);
-//         } else {
-//           i--;
-//         }
-//       }
-// }
-
-// function ordenaJogo() {
-//   novoJogoOrdenado = novoJogo.sort(function(a,b) {
-//     if(a > b) return 1;
-//     if(a < b) return -1;
-//     return 0;
-//   });
-// }
-
-// function mostraJogoNaTela() {
-//   for(let i = 0; i < 6; i++) {
-//     numero[i].innerHTML = novoJogoOrdenado[i];
-//   }
-// }
-
-// function salvarJogo()  {
-//   if(!jogosSalvos.includes(novoJogoOrdenado)) {
-//     novoJogoOrdenado.id = jogosSalvos.length;
-//     jogosSalvos.push(novoJogoOrdenado);
-    
-//     localStorage.setItem("jogosSalvos", JSON.stringify(jogosSalvos));
-    
-//     const novoJogoString = novoJogoOrdenado.toString();
-//     const jogoArrumado = novoJogoString.replaceAll(',',' - ');
-    
-//     const novoItem = document.createElement('li');
-//     const meusNumerosItem = document.createElement('p');
-//     meusNumerosItem.classList.add('meus-jogos__numeros');
-//     meusNumerosItem.innerHTML = jogoArrumado;
-    
-//     novoItem.appendChild(meusNumerosItem);
-//     novoItem.appendChild(botaoDeleta(novoJogoOrdenado.id));
-//     meusJogosLista.appendChild(novoItem);
-//   } 
-// }
-
-// function botaoDeleta(id) {
-//   const elementoBotao = document.createElement("button");
-//   elementoBotao.innerText = "X";
-
-//   elementoBotao.addEventListener("click", function() {
-//     deletaElemento(this.parentNode, id);
-//   })
-
-//   return elementoBotao;
-// }
-
-// function deletaElemento(tag, id) {
-//   tag.remove();
-
-//   jogosSalvos.splice(jogosSalvos.findIndex(elemento => elemento.id === id), 1);
-// }
